3faef8f58021e8bf8852f44816287920c649575a
Eloff <dan.eloff@gmail.com>
2021-04-10 18:46:24 -0700

    remove old stuff

--- flowstate-example-react/hello-world.js
+++
@@ -1,5 +0,0 @@
-// my-first-snowpack/hello-world.js
-export function helloWorld() {
-  let header = document.getElementById("header");
-  header.innerText = "Hello, World!";
-}
--- flowstate-example-react/snowpack.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    "plugins": [
-        "flowstate-snowpack-plugin"
-    ],
-    "devOptions": {
-        "open": "none"
-    }
-};
